7a7a86dc5f223e73b65179ce65cd62fce693650d
井上沙耶 <scomp@MacBook-Pro.local>
2018-11-10 16:25:00 +0900

    First  commit

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module ChatSpace
+  class Application < Rails::Application
+    config.generators do |g|
+    g.stylesheets false
+    g.javascripts false
+    g.helper false
+    g.test_framework false
+    end
+  end
+end
