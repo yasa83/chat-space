da0f6f7cbe0198919f4474aa9602a4f73e628b8d
Your Name <you@example.com>
2018-12-06 19:12:59 +0900

    Create-user-manage-function
    Lesson9-chapter1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root        'groups#index'
+  root        'messages#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-     resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :create]
+  end
 end
