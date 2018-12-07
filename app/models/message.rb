d9cfc5a75e616981238dd4156b4af2940c46e076
Your Name <you@example.com>
2018-12-07 12:45:53 +0900

    Side-bar-group-message-function

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  # mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
   validates :content, presence: true, unless: :image?
 end
