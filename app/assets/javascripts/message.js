055a108e3d2ca14805cae88cfa203b87d33fa659
Your Name <you@example.com>
2018-12-14 14:57:42 +0900

    [Add]ajaxmessage-controller

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,15 @@
 $(function(){
-  alert("a")
-  $('.new_message').on('submit', function(e){
-    console.log('pushed submit button');
-     e.preventDefault();
-  });
+  $('#new_message').on('submit', function(e){
+    ve.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
 });
-
