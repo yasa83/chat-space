a1249eff4b14a56e795fd3de5ab1c57144cffaec
Your Name <you@example.com>
2018-12-20 00:48:15 +0900

    [Add] -error-message
    一致するユーザーがいないときの表示

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,18 +1,26 @@
 $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-
     $.ajax({
       type: 'GET',
       url: '/users',
       data: { keyword: input },
       dataType: 'json'
     })
-    // .done(function(users){
-
-    // })
-    // .fail(function(user){
-    //   alert('メッセージか画像を入力してください。');
-    // });
+    .done(function(users){
+      $('#user-search-result').empty();
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendUser(user)
+        });
+      }
+      else{
+        $('#user-search-result').empty();
+        appendNoUser("一致するユーザーはいません")
+      }
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
+    });
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
   def index
-    binding.pry
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
      respond_to do |format|
      format.html
