e3cdb1aa41d4fc3c6ca3ede0ac916c988d670358
Your Name <you@example.com>
2018-12-20 08:46:36 +0900

    [Add] search-function
    インクリメンタルサーチで候補のユーザーを表示できるようにした

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,18 @@
 $(function() {
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    $('#user-search-result').append(html)
+  }
+  function appendNoUser(alert){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${alert}</p>
+                </div>`
+    $("#user-search-result").append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     $.ajax({
@@ -22,5 +36,6 @@ $(function() {
     .fail(function(){
       alert('ユーザー検索に失敗しました');
     });
+
   });
 });
