e13ed5db3fd76feac700275c10eb42a24a53fa41
Your Name <you@example.com>
2018-12-21 14:24:58 +0900

    Fix-user-search-function
    検索したときログインユーザーは候補に含めない。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20).where.not(id: current_user.id)
      respond_to do |format|
      format.html
      format.json
