05e4940623682890583982cc387650535154e187
Your Name <you@example.com>
2018-12-26 16:08:08 +0900

    Clean-image-view

--- app/assets/javascripts/auto_message.js
+++ app/assets/javascripts/auto_message.js
@@ -1,6 +1,9 @@
 $(function(){
   function buildHTML(message) {
-    var message_image = `<img class: 'lower-message__image' src='${message.image}'>`
+    var message_image = ""
+    if(message.id){
+      var message_image = `<img class: 'lower-message__image' src='${message.image}'>`
+    }
     var html = `<div class="chat-main__body--message"  data-message-id="${message.id}">
                   <div class="chat-main__body--message-name">
                     ${message.name}
@@ -12,13 +15,10 @@ $(function(){
                     <p class="lower-message__content">
                       ${message.content}
                     </p>
+                    ${message_image}
                   </div>
                 </div>`
-    if(message.image){
-      return html + message_image;
-    }else{
       return html;
-    }
   }
   var Interval = setInterval(function(){
       if (location.href.match(/\/groups\/\d+\/messages/)){
