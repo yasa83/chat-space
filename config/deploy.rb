016eb52b42a8a652d764a3aa6bc765cfc89f6f14
Your Name <you@example.com>
2019-01-03 13:08:34 +0900

    Reverse-deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :keep_releases, 5
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-  append :linked_files, 'config/database.yml', 'config/master.key'
+  # append :linked_files, 'config/database.yml', 'config/master.key'
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
